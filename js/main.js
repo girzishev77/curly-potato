d3d6690cce001d194b14f6e508c02038e2d8839c
Adam Girzishev <girzishev99@bk.ru>
2023-01-23 18:46:36 +0300

    curly-potato
    curly-potato

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Что бы задать дату формата нужно использовать тип данных number
+//Для сохранения числа ПИ нужен Math.PI
+let name = prompt("Как тебя зовут");
+let address = prompt("Где ты живешь");
+let phone = prompt("Твой номер телефона");
+console.log(
+  `Меня зовут ${name} , мой адрес ${address},мой номер телефона ${phone}  `
+);
+
+let r = 300;
+let $ = 68;
+console.log(r / $);
+const admin = Max;
+const admin = Alice;
